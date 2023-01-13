b2f78f98f16331ce51c0196204fd1a1724663818
Yusuf <y.abdulla37@gmail.com>
2023-01-13 06:37:48 -0700

    added form page and fixed form bug

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -3,6 +3,7 @@ import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
+// import { defaultAnimate } from "@nivo/core";
 
 const initialValues = {
   firstName: "",
@@ -31,15 +32,15 @@ const Form = () => {
   }
 
   return (
-    <Box m="20px">
+    <Box m="20px" >
         <Header title="CREATE USER" subtitle="Create a New User Profile" />
 
         <Formik
           onSubmit={handleFormSubmit}
           initialValues={initialValues}
           validationSchema={userSchema}
-        />
-        {({ value, errors, touched, handleBlur, handleChange, handleSubmit}) => (
+        >
+        {({ values, errors, touched, handleBlur, handleChange, handleSubmit}) => (
           <form onSubmit={handleSubmit}>
             <Box 
             display="grid" 
@@ -55,6 +56,7 @@ const Form = () => {
               type="text"
               label="First Name"
               onBlur={handleBlur}
+              onChange={handleChange}
               value={values.firstName}
               name="firstName"
               error={!!touched.firstName && !!errors.firstName}
@@ -62,10 +64,84 @@ const Form = () => {
               sx={{ gridColumn: "span 2"}}
               />
 
+              <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Last Name"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.lastName}
+              name="lastName"
+              error={!!touched.lastName && !!errors.lastName}
+              helperText={touched.lastName && errors.lastName}
+              sx={{ gridColumn: "span 2"}}
+              />
+
+
+              <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={!!touched.email && !!errors.email}
+              helperText={touched.email && errors.email}
+              sx={{ gridColumn: "span 4"}}
+              />
+
+              <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Contact Number"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.contact}
+              name="contact"
+              error={!!touched.contact && !!errors.contact}
+              helperText={touched.contact && errors.contact}
+              sx={{ gridColumn: "span 4"}}
+              />
+
+              <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Address 1"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.address1}
+              name="address1"
+              error={!!touched.address1 && !!errors.address1}
+              helperText={touched.address1 && errors.address1}
+              sx={{ gridColumn: "span 4"}}
+              />
+
+              <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Address 2"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.address2}
+              name="address2"
+              error={!!touched.address2 && !!errors.address2}
+              helperText={touched.address2 && errors.address2}
+              sx={{ gridColumn: "span 4"}}
+              />
+
             </Box>
           </form>
         )
         }
+        </Formik>
     </Box>
   )
 }
+
+export default Form;
