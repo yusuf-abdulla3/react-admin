68eb740fd263003fdda6390c4509e15b1d532a79
Yusuf <y.abdulla37@gmail.com>
2023-01-20 22:21:39 -0700

    fix fixed typo

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -1,6 +1,6 @@
 import { useTheme, Box, Typography } from "@mui/material";
 import { tokens } from "../theme";
-import ProgressCirle from "./ProgressCircle";
+import ProgressCircle from "./ProgressCircle";
 
 const StatBox = ({title, subtitle, icon, progress, increase}) => {
 const theme = useTheme();
@@ -11,24 +11,30 @@ return(
     <Box display="flex" justifyContent="space-between">
       <Box>
         {icon}
-        <Typography variant="h4" fontWeight="bold" sx={{ color: colors.grey[100]}}>
+        <Typography 
+          variant="h4" 
+          fontWeight="bold" 
+          sx={{ color: colors.grey[100]}}>
             {title}
         </Typography>
       </Box>
       <Box>
-        <ProgressCirle progress = {progress}/>
+        <ProgressCircle progress = {progress} />
+      </Box>
       </Box>
       <Box display="flex" justifyContent="space-between">
       <Typography variant="h5" sx={{ color: colors.greenAccent[500]}}>
-            {subtitle}
-        </Typography>
-        <Typography variant="h5" fontStyle="italic" sx={{ color: colors.greenAccent[600]}}>
-            {increase}
-        </Typography>
+          {subtitle}
+      </Typography>
+      <Typography 
+        variant="h5" 
+        fontStyle="italic" 
+        sx={{ color: colors.greenAccent[600] }}>
+          {increase}
+      </Typography>
       </Box>
     </Box>
-  </Box>
-)
+  )
 }
 
 export default StatBox;
